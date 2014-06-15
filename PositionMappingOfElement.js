7024d3eba32f7c8cf356ce555718d03ca0e77f6e
FukuyamaShingo <houseshou@FukuyamaShingo-no-MacBook-Pro.local>
2014-06-15 22:07:42 +0900

    Improve UI

--- PositionMappingOfElement.js
+++ PositionMappingOfElement.js
@@ -21,7 +21,7 @@
 })(function($) {
 
   $.fn.flexibleElem = function() {
-    return '<div class="flexibleElem" style="width:60px;height:60px;z-index:999999;position:absolute;background:rgba(255,0,0,0.2);cursor:move;display:none;">'+ '' +'</div>';
+    return '<div class="flexibleElem" style="width:60px;height:60px;top:0;left:0;z-index:999999;position:absolute;background:rgba(255,0,0,0.2);cursor:move;display:none;">'+ '' +'</div>';
   };
 
   var $body = $('body');
@@ -35,15 +35,15 @@
   $flexibleController.css({
     width:'130px',
     height:'150px',
-    border:'1px solid #999',
+    border:'1px solid #ccc',
     borderRadius:'15px',
     zIndex:1000000,
     position:'fixed',
     bottom:0,
     left:0,
-    background:'#fff',
     padding:'15px',
-    cursor:'move'
+    cursor:'move',
+    background:'rgba(255,255,255,0.9)'
   });
   var $flexibleAdd = $flexibleController.find('#flexibleAdd');
   $flexibleAdd.css({
@@ -56,8 +56,9 @@
     height:'40px',
     padding:'5px',
     borderRadius:'40px',
-    background:'none',
-    display:'block'
+    boxShadow:'2px 3px 0px -2px #ccc, -2px -3px 0px -2px #eee',
+    display:'block',
+    background:'none'
   }).hide();
   var $textarea = $flexibleController.find('textarea');
   $textarea.css({
@@ -68,8 +69,10 @@
     width:'120px',
     height:'80px',
     margin:'0 auto 15px',
-    padding:'5px'
+    padding:'5px',
+    background:'none'
   });
+
   $flexibleController.draggable();
 
   $flexibleAdd.on('click', function(e) {
@@ -111,22 +114,21 @@
   };
 
   $(document).on('drag.r dragstart.r dragstop.r', '.flexibleElem', function(e, ui) {
-    var $t = $(this);
-    var $p = $t.parent();
-    var _top = Math.round(ui.position.top);
-    var _left = Math.round(ui.position.left);
-    var _width = $t.width();
+    var $t      = $(this);
+    var $p      = $t.parent();
+    var _top    = Math.round(ui.position.top);
+    var _left   = Math.round(ui.position.left);
+    var _width  = $t.width();
     var _height = $t.height();
     $textarea.val('top: '+_top+'px;\nleft: '+_left+'px;\n'+'width: '+_width+'px;\nheight: '+_height+';');
     if (e.type === 'dragstart') $p.css({outline: '1px solid #f90'});
     if (e.type === 'dragstop')  $p.css({outline: 'none'});
   }).on('resize.r resizestart.r resizestop.r', function(e, ui) {
-    var $t = ui.element;
-    var $p = $t.parent();
-    console.log($t[0].tagName);
-    var _top = ui.position.top;
-    var _left = ui.position.left;
-    var _width = ui.size.width;
+    var $t      = ui.element; // ! Not $(this)
+    var $p      = $t.parent();
+    var _top    = ui.position.top;
+    var _left   = ui.position.left;
+    var _width  = ui.size.width;
     var _height = ui.size.height;
     $textarea.val('top: '+_top+'px;\nleft: '+_left+'px;\n'+'width: '+_width+'px;\nheight: '+_height+';');
     if (e.type === 'resizestart') $p.css({outline: '1px solid #f90'});
